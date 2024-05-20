7ac6110a9d7b7a4158fc6499e8f6f2c18565c491
VaalGeek <ranyaole@yahoo.com>
2024-05-20 14:27:42 +0200

    added hunddle landing page challenge

---
+++ pages/Huddle.vue
@@ -0,0 +1,37 @@
+<template>
+    <div
+        class="bg-[#6A4DB2] min-h-screen sm:bg-[url('/assets/huddle/bg-desktop.svg')]   bg-[url('/assets/huddle/bg-mobile.svg')] bg-no-repeat">
+        <img class="py-9 w-[200px] px-9" src="/assets/huddle/logo.svg" alt="">
+
+        <div class="grid sm:grid-cols-2 sm:gap-0">
+            <img class="mt-5 sm:mt-24 sm:pl-40 px-9 " src="/assets/huddle/illustration-mockups.svg" alt="">
+            <div class="sm:max-w-xl">
+                <h1 class="text-2xl mt-12  px-12 text-center sm:px-0 sm:text-4xl sm:text-left text-white font-semibold">Build The Community <br class="hidden sm:block"> Your Fans Will
+                    Love
+                </h1>
+                <p class="text-white sm:text-left  font-thin text-center text-wrap px-12 sm:px-0 mt-4">Huddle re-imagines the way we build
+                    communities. You have a voice, <br class="hidden sm:block"> but so does your audience.
+                    Create
+                    connections with your users as you engage in <br> genuine discussion.</p>
+                <div class="flex justify-center sm:justify-start">
+                    <button
+                        class="bg-white hover:bg-[#E882E8] delay-75 hover:text-white sm:py-3 sm:text-sm  text-xs py-2 rounded-full text-center text-[#6A4DB2] px-16 mt-5 shadow-lg">Register</button>
+                </div>
+                <div
+                    class="flex sm:py-0 py-10 sm:mb-2 sm:absolute sm:right-72 bottom-0 flex-row  cursor-pointer justify-center sm:justify-end gap-3  lg:py-28 sm:mr-1 lg:mr-10">
+                    <div class="rounded-full hover:bg-[#E882E8] delay-75 hover:text-white  size-7  text-center   ring-white ring-1">
+                        <Icon class="text-white   text-sm" name="fa6-brands:facebook-f"></Icon>
+                    </div>
+                    <div class="rounded-full size-7 hover:bg-[#E882E8] delay-75 hover:text-white  text-center ring-white ring-1">
+                        <Icon class="text-white text-sm " name="fa6-brands:twitter"></Icon>
+                    </div>
+                    <div class="rounded-full size-7 hover:bg-[#E882E8] delay-75 hover:text-white  text-center  ring-white ring-1">
+                        <Icon class="text-white text-sm " name="fa6-brands:instagram"></Icon>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+</template>
