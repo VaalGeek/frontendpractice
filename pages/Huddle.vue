c85048a990d81fcd4f7c63f9315d9590225ed216
VaalGeek <ranyaole@yahoo.com>
2024-05-20 21:05:12 +0200

    made changes

--- pages/Huddle.vue
+++ pages/Huddle.vue
@@ -1,24 +1,24 @@
 <template>
     <div
-        class="bg-[#6A4DB2] min-h-screen sm:bg-[url('/assets/huddle/bg-desktop.svg')]   bg-[url('/assets/huddle/bg-mobile.svg')] bg-no-repeat">
-        <img class="py-9 w-[200px] px-9" src="/assets/huddle/logo.svg" alt="">
+        class="bg-[#6A4DB2]   min-h-screen  lg:bg-auto  lg:bg-[url('/assets/huddle/bg-desktop.svg')]   bg-[url('/assets/huddle/bg-mobile.svg')] bg-no-repeat">
+        <img class="py-9 sm:mx-16 sm:w-[250px]  w-[200px] px-9" src="/assets/huddle/logo.svg" alt="">
 
-        <div class="grid sm:grid-cols-2 sm:gap-0">
-            <img class="mt-5 sm:mt-24 sm:pl-40 px-9 " src="/assets/huddle/illustration-mockups.svg" alt="">
-            <div class="sm:max-w-xl">
-                <h1 class="text-2xl mt-12  px-12 text-center sm:px-0 sm:text-4xl sm:text-left text-white font-semibold">Build The Community <br class="hidden sm:block"> Your Fans Will
+        <div class="grid lg:grid-cols-2 lg:max-w-fit lg:gap-24  max-w-md sm:max-w-xl">
+            <img class="mt-5 sm:mt-0 lg:mx-24     lg:py-20 sm:mx-20 px-9 " src="/assets/huddle/illustration-mockups.svg" alt="">
+            <div class="flex flex-col">
+                <h1 class="text-2xl mt-12 lg:mt-28 sm:mt-0  px-12 sm:px-0 sm:text-center sm:text-wrap text-center lg:px-10 lg:text-4xl lg:text-left text-white font-semibold">Build The Community <br class="hidden sm:block"> Your Fans Will
                     Love
                 </h1>
-                <p class="text-white sm:text-left  font-thin text-center text-wrap px-12 sm:px-0 mt-4">Huddle re-imagines the way we build
-                    communities. You have a voice, <br class="hidden sm:block"> but so does your audience.
+                <p class="text-white sm:text-center sm:mx-10    font-thin text-center text-wrap px-9 lg:px-2 lg:text-left sm:mt-2 lg:mt-4  mt-4">Huddle re-imagines the way we build
+                    communities. You have a voice, <br class="hidden lg:block"> but so does your audience.
                     Create
-                    connections with your users as you engage in <br> genuine discussion.</p>
-                <div class="flex justify-center sm:justify-start">
+                    connections with your users as you engage in  genuine discussion.</p>
+                <div class="flex justify-center sm:mx-10 lg:justify-start">
                     <button
-                        class="bg-white hover:bg-[#E882E8] delay-75 hover:text-white sm:py-3 sm:text-sm  text-xs py-2 rounded-full text-center text-[#6A4DB2] px-16 mt-5 shadow-lg">Register</button>
+                        class="bg-white hover:bg-[#E882E8] delay-75 hover:text-white lg:py-3  lg:text-sm  text-xs py-2 rounded-full text-center text-[#6A4DB2] px-16 mt-5 shadow-lg">Register</button>
                 </div>
                 <div
-                    class="flex sm:py-0 py-10 sm:mb-2 sm:absolute sm:right-72 bottom-0 flex-row  cursor-pointer justify-center sm:justify-end gap-3  lg:py-28 sm:mr-1 lg:mr-10">
+                    class="flex sm:py-0 py-10 sm:mb-2 lg:absolute lg:right-8  bottom-0 flex-row  cursor-pointer justify-center sm:justify-end gap-3  lg:py-28 sm:mr-1 lg:mr-10">
                     <div class="rounded-full hover:bg-[#E882E8] delay-75 hover:text-white  size-7  text-center   ring-white ring-1">
                         <Icon class="text-white   text-sm" name="fa6-brands:facebook-f"></Icon>
                     </div>
